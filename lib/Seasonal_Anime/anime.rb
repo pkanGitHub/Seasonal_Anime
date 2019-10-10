f8a5ce6fe655530fde24c895166f97036edd0342
Phyllis Kan <pyk9q7@gmail.com>
2019-10-10 23:20:53 +0000

    updates

--- lib/Seasonal_Anime/anime.rb
+++ lib/Seasonal_Anime/anime.rb
@@ -11,4 +11,22 @@ class Anime
     @@all
   end
 
+  def self.winter_anime
+    @@all.select{|anime| anime.season == "winter"}
+  end
+
+  def self.spring_anime
+    puts "List of Spring anime"
+    @@all.select{|anime| anime.season == "spring"}
+  end
+
+  def self.summer_anime
+    puts "List of Summer anime"
+    @@all.select{|anime| anime.season == "summer"}
+  end
+
+  def self.fall_anime
+    puts "List of Fall anime"
+    @@all.select{|anime| anime.season == "fall"}
+  end
 end
--- lib/Seasonal_Anime/cli.rb
+++ lib/Seasonal_Anime/cli.rb
@@ -1,13 +1,95 @@
 # Our CLI Controller
 
-# class SeasonalAnime::CLI
-#
-#   def call
-#     list_years
-#     menu
-#     goodbye
+class SeasonalAnime::CLI
+
+  def call
+      year = year_menu
+      season_menu(year)
+      Scraper.periods
+  end
+
+  # class SeasonalAnime::CLI
+  #
+  #   def call
+  #     list_years
+  #     menu
+  #     goodbye
+  #   end
+
+  def year_menu
+    year = nil
+    # while input != exit
+      puts "Please enter a year: "
+      year = gets.strip
+      # puts "If you want to quit, type exit."
+  end
+
+  def season_menu(year)
+    season = nil
+      puts "List of Seasons:"
+
+      puts "1. Winter"
+      puts "2. Spring"
+      puts "3. Summer"
+      puts "4. Fall"
+
+      puts "Please select one season: "
+      season = gets.strip
+
+      case season
+      when "1"
+        winter_anime(year)
+      when "2"
+        spring_anime
+      when "3"
+        self.summer_anime
+      when "4"
+        fall_anime
+      when "exit"
+        # puts "See you next time!"
+      else
+        # puts "Please only select from the menu: "
+        # season_menu
+      end
+    end
+
+      def winter_anime(year)
+        puts "List of Winter anime"
+        period = Scraper.periods.detect{|period| period.season == "Winter" && period.year == year}
+        binding.pry
+        Anime.all
+      end
+
+      def spring_anime
+        puts "List of Spring anime"
+      end
+
+      def summer_anime
+        puts "List of Summer anime"
+      end
+
+      def fall_anime
+        puts "List of Fall anime"
+      end
+
+end
+
+    # period.url = anchor.attribute("href").value
+    # period.season = anchor.text.strip.split[0]
+    # period.year = anchor.text.strip.split[1]
+
+    # def menu
+    #   input = nil
+    #   while input != "exit"
+        # puts "Enter the number for the period you are looking for or type list to see again (or type exit): "
+
+        # puts "Enter the season and year you want to search, ex: 'Summer 2015'"
+        # input = gets.strip.downcase
+        # input.split(" ")[1] # 2015
+        # case input
 #   end
 #
+# end
 #   def list_years
 #     # here doc
 #     puts "List of Years:"
