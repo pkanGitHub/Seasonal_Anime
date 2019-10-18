8b1b5025eca4a47a2b2d126c318a2e14c80fa985
Phyllis Kan <Phyllis Kan>
2019-10-18 13:56:07 -0500

    Final

--- lib/Seasonal_Anime/cli.rb
+++ lib/Seasonal_Anime/cli.rb
@@ -9,27 +9,30 @@ class SeasonalAnime::CLI
 
   def year_menu
     year = nil
-      puts "Please enter a year: "
-      year = gets.strip.to_i
-      # if year < 1917    # NO Method Error
-      #   puts "Sorry nothing before 1917 is available, please try again."
-      #   year_menu
-      # elsif year > 2019
-      #   puts "Sorry, the anime over 2019 aren't out yet, please enter the year within the range 1917 and 2019:"
-      #   year_menu
-      # end
+    puts "\nPlease enter a year: "
+    year = gets.strip.to_i
+    if year < 1917    # NO Method Error
+      puts "Sorry nothing before 1917 is available, please try again."
+      year_menu
+    elsif year > 2019
+      puts "Sorry, the anime over 2019 aren't out yet, please enter the year within the range 1917 and 2019:"
+      year_menu
+    else
+      puts "\nHow about seasons?"
+    end
+    year
   end
 
   def season_menu(year)
     season = nil
       puts "List of Seasons:"
 
-      puts "1. Winter"
+      puts "\n1. Winter"
       puts "2. Spring"
       puts "3. Summer"
       puts "4. Fall"
 
-      puts "Please select one season: "
+      puts "\nPlease select one season: "
       season = gets.strip
 
       case season
@@ -51,24 +54,25 @@ class SeasonalAnime::CLI
 
     def select_an_anime_for_detail(period)
       anime_array = period.animes
-      puts "Please select the number of the anime information you want to see: "
+      puts "\nPlease select the number of the anime information you want to see: "
       input = gets.strip.to_i - 1 # index value 0 - max_input
       max_inputs = anime_array.size
 
       if input < max_inputs
         anime = anime_array[input] # The 1 anime object that user selected
+        puts "\n"
         show_anime_details(anime)
       else
-        puts "Something went wrong, please only input number from 1 - #{max_inputs}"
+        puts "Something went wrong !!! Please only input number from 1 - #{max_inputs}"
         select_an_anime_for_detail(period)
       end
 
     end
 
       def season_anime(season, year)
-        puts "List of #{season} anime:"
-        # Period.make_anime_list
+        puts "\nList of #{season} anime:"
         period = Period.find_by_season_and_year(season, year)
+        puts "#{season}, #{year}\n\n"
         period.animes.each.with_index(1) do |anime, i|
           puts "#{i}. #{anime.title}"
         end
@@ -84,5 +88,4 @@ class SeasonalAnime::CLI
           puts "Source: #{anime.source}"
           puts "Synopsis: #{anime.synopsis}"
       end
-
 end
